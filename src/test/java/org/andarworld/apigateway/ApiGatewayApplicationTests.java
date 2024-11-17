d86c3e485772993ad7c07fe7d84e3fd1d175fa61
Adrian <andarworld@yandex.ru>
2024-11-17 17:47:15 +0400

    Start of work

---
+++ src/test/java/org/andarworld/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package org.andarworld.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
